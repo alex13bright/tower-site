26ff5a6109843e2619f52dfed441ee985e232871
alex13bright <alex13bright@gmail.com>
2022-06-03 20:26:49 +0700

    extract Document element to prepare for Error/Catch Boundaries getting locale from request header

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,17 @@
-import type { MetaFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import type { ReactNode } from 'react';
+import type { LoaderFunction, MetaFunction } from '@remix-run/node';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react';
 import { DynamicLinks } from 'remix-utils';
+import type { Locale } from '~/api-types/main';
+import { json } from '@remix-run/node';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -8,20 +19,47 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
-export default function App() {
+type LoaderData = {
+  locale: Locale;
+};
+export const loader: LoaderFunction = async ({ request }) => {
+  const headers = request.headers;
+  const acceptLanguages = headers.get('Accept-Language');
+  if (acceptLanguages) {
+    const [locale] = acceptLanguages.split(',');
+    if (locale === 'en' || locale === 'ru') {
+      return json<LoaderData>({ locale: locale as Locale });
+    }
+  }
+  // in future at least fall back to en locale
+  throw new Error(`can't get locale from 'Accept-Language' request header`);
+};
+
+export default function Root() {
+  const { locale } = useLoaderData();
   return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <DynamicLinks />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <Document locale={locale}>
+      <Outlet />
+    </Document>
   );
 }
+
+type DocumentProps = {
+  locale: Locale;
+  children: ReactNode;
+};
+const Document = ({ locale }: DocumentProps) => (
+  <html lang={locale}>
+    <head>
+      <Meta />
+      <DynamicLinks />
+      <Links />
+    </head>
+    <body>
+      <Outlet />
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
+);
