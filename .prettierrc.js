ec078f84feae55d5666dd9e813e924df639b0716
alex13bright <alex13bright@gmail.com>
2022-06-03 15:21:09 +0700

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
