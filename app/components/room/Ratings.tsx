29ae6ffd49cac6a7407c1412e8889b75b834a5f6
alex13bright <alex13bright@gmail.com>
2022-06-13 21:14:05 +0700

    fix ratings

--- app/components/room/Ratings.tsx
+++ app/components/room/Ratings.tsx
@@ -5,10 +5,16 @@ const Main = styled.div`
   align-items: center;
   gap: 10px;
 `
-const Stars = styled.div<{ number: number }>`
+const Mixer = styled.div`
+  display: grid;
+  grid-template-areas: 'content';
+`
+const Stars = styled.div<{ number: number; illuminated: boolean }>`
+  grid-area: content;
   background-image: url(/images/ratings-star.svg);
   background-repeat: repeat-x;
   width: ${({ number }) => number * 24}px;
+  background-position: 0 ${({ illuminated }) => (illuminated ? 0 : '100%')};
   height: 16px;
 `
 const Number = styled.span`
@@ -24,7 +30,10 @@ const Unit = styled.span`
 `
 export const Ratings = ({ ratings, className }: { ratings: number; className?: string }) => (
   <Main className={className}>
-    <Stars number={ratings} />
+    <Mixer>
+      <Stars number={5} illuminated={false} />
+      <Stars number={ratings} illuminated={true} />
+    </Mixer>
     <Number>
       <Text>{ratings}</Text>
       <Unit>/5</Unit>
