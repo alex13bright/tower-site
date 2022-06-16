fe2be2213b2a6c436bb3968894fec54e1ac44e54
alex13bright <alex13bright@gmail.com>
2022-06-16 16:19:58 +0700

    fix spoiler arrow direction

--- app/components/Spoiler.tsx
+++ app/components/Spoiler.tsx
@@ -3,20 +3,22 @@ import styled from 'styled-components'
 
 const Main = styled.div`
   display: grid;
-  grid-template-columns: 1fr auto;
-  column-gap: 10px;
+  grid-template-columns: 1fr 0;
   align-items: center;
+  position: relative;
 `
 
 const Container = styled.div<{ activeHeight: string }>`
   overflow: hidden;
   height: ${({ activeHeight }) => activeHeight};
 `
-const Button = styled.button<{ height: string; isPressed: boolean }>`
+const Button = styled.button<{ height: string; $hidden: boolean }>`
+  width: 20px;
+  margin-right: -20px;
   height: ${({ height }) => height};
   aspect-ratio: 1 / 1;
   background: url(/images/arrow-down.svg) no-repeat 50%;
-  ${({ isPressed }) => (isPressed ? `transform: rotateX(180deg)` : '')};
+  ${({ $hidden }) => ($hidden ? '' : `transform: rotateX(180deg)`)};
 `
 
 type Props = {
@@ -25,16 +27,12 @@ type Props = {
 }
 
 export function Spoiler({ height, children }: Props): ReactElement {
-  const [hidden, setHidden] = useState<boolean>(false)
+  const [hidden, setHidden] = useState<boolean>(true)
   const activeHeight = hidden ? height : 'auto'
   return (
     <Main>
       <Container activeHeight={activeHeight}>{children}</Container>
-      <Button
-        height={height}
-        isPressed={hidden}
-        onClick={() => setHidden((hidden) => !hidden)}
-      ></Button>
+      <Button height={height} $hidden onClick={() => setHidden((hidden) => !hidden)}></Button>
     </Main>
   )
 }
