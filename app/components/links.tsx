27ef0816c56e45c2477a58d60b627dacd34df9bd
alex13bright <alex13bright@gmail.com>
2022-06-18 04:14:00 +0700

    fixes

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -1,6 +1,6 @@
 import { AnchorHTMLAttributes, ReactElement } from 'react'
 
-export const ExternalLink = (props: AnchorHTMLAttributes<unknown>): ReactElement => {
+export const ExternalLink = (props: AnchorHTMLAttributes<HTMLElement>): ReactElement => {
   return (
     <a {...props} target="_blank" rel="noreferer nofollow noopener">
       {props.children}
