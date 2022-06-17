050fd7980abff7f8caa4026555c94ffe0ed2379a
alex13bright <alex13bright@gmail.com>
2022-06-17 14:49:35 +0700

    moderate version of model window implementation

--- app/components/ModalWindow.tsx
+++ app/components/ModalWindow.tsx
@@ -1,31 +1,29 @@
 import { Dispatch, ReactElement, ReactNode, SetStateAction } from 'react'
 import styled from 'styled-components'
-import { background, breakpoints, primary, widthAtLeast } from '~/styles/styles'
+import { background, breakpoints, primary } from '~/styles/styles'
 import { Cross } from '~/components/Cross'
 
-const containerPaddings = {
-  right: 20,
-}
+const ReCross = styled(Cross)`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  opacity: 0.3;
+`
+
 const Container = styled.div<{ isVisible: boolean }>`
-  display: ${({ isVisible }) => (isVisible ? 'grid' : 'none')};
   position: fixed;
-  width: 100%;
-  height: 100%;
   top: 0;
   left: 0;
   z-index: 9999;
+  width: 100%;
+  height: 100%;
   cursor: pointer;
   background: rgba(34, 34, 34, 0.5);
-  padding: ${containerPaddings.right}px;
+  padding: 20px;
+  display: ${({ isVisible }) => (isVisible ? 'grid' : 'none')};
   place-items: center;
-  overflow-x: auto;
-  min-width: ${breakpoints.xs}px;
 `
 
-const contentPaddings = {
-  right: 13,
-  left: 30,
-}
 const Content = styled.div`
   width: 100%;
   height: 100%;
@@ -35,33 +33,20 @@ const Content = styled.div`
   border-radius: 10px;
   box-shadow: 0 15px 50px rgb(0 0 0 / 50%);
   position: relative;
-  padding: 45px ${contentPaddings.right}px 15px ${contentPaddings.left}px;
+  padding: 50px 20px 20px 20px;
   max-width: ${breakpoints.md}px;
   overflow: hidden;
 `
+
 const ScrollArea = styled.div`
   padding: 10px;
   width: 100%;
   height: 100%;
-  overflow-y: auto;
+  overflow: auto;
 `
-const ReCross = styled(Cross)`
-  position: absolute;
-  top: 15px;
-  right: 15px;
-  opacity: 0.3;
-`
-const Wrapper = styled.div`
-  width: ${breakpoints.xs -
-  containerPaddings.right * 2 -
-  contentPaddings.right +
-  contentPaddings.left}px;
-  @media ${widthAtLeast.md} {
-    width: calc(100vw - 140px);
-  }
-  @media (min-width: calc(${breakpoints.md + 80}px)) {
-    width: calc(${breakpoints.md - 80}px);
-  }
+
+const ScrollAreaContent = styled.div`
+  min-width: ${breakpoints.xs - 120}px;
 `
 
 type Props = {
@@ -77,7 +62,7 @@ export function ModalWindow({ children, isVisible, setIsVisible }: Props): React
       <Content onClick={(e) => e.stopPropagation()}>
         <ReCross length={15} depth={2} onClick={handleClick} color="#555" />
         <ScrollArea>
-          <Wrapper>{children}</Wrapper>
+          <ScrollAreaContent>{children}</ScrollAreaContent>
         </ScrollArea>
       </Content>
     </Container>
