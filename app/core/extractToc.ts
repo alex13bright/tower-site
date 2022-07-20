3fccb7fe52f3d2ee389dc379a8f6dab230a8d757
alex13bright <alex13bright@gmail.com>
2022-07-20 10:38:06 +0700

    next

--- app/core/extractToc.ts
+++ app/core/extractToc.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import { JSDOM } from 'jsdom'
 
 import { TocModeType, TocType } from '~/core/types'
@@ -7,17 +6,15 @@ import { slugify } from '~/core/singletons'
 export const extractToc = (content: string, tocMode: TocModeType): TocType => {
   const root = JSDOM.fragment(`<div>${content}</div>`)
   const titles: { title: string; slug: string | null }[] = []
-  const tags: HTMLElement[] = root.querySelectorAll('h2')
+  const tags: NodeListOf<HTMLElement> = root.querySelectorAll('h2')
   tags.forEach((tag) => {
     const title = tag.innerHTML
     const slug = null
     titles.push({ title, slug })
   })
 
-  const toc = titles.map(({ title, slug }) => ({
+  return titles.map(({ title, slug }) => ({
     title,
     id: slug !== null ? slug : `${slugify(title)}`,
   }))
-
-  return toc
 }
